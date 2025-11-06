626d23d386361fc3b7d3c1a2cfc509e78c2066f8
Jaden <jadenwrightmaurais@gmail.com>
2025-11-05 20:28:13 -0500

    Fixed error

--- Backend/src/main/java/com/trainapp/services/TicketService.java
+++ Backend/src/main/java/com/trainapp/services/TicketService.java
@@ -1,12 +1,14 @@
 package com.trainapp.services;
 
+import com.trainapp.model.Connection;
 import com.trainapp.model.Ticket;
 
 public class TicketService {
     public TicketService(){
     }
 
-    public Ticket createTicket(){
-
+    public Ticket createTicket(Connection connection){
+        Ticket ticket = new Ticket(connection.getArrivalCity(), connection.getDepartureCity());
+        return ticket;
     }
 }
